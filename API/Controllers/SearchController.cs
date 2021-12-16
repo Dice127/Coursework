48aef494a1c78b59bfd02cb3ab6a6028ab4bac41
Dice127 <ghjhj127@gmail.com>
2021-12-16 21:38:24 +0200

    fixed exception

--- API/Controllers/SearchController.cs
+++ API/Controllers/SearchController.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using Models;
 using Mappers;
 using Services;
 using System.Linq;
+using System;
 
 namespace API.Controllers
 {
@@ -16,51 +18,66 @@ namespace API.Controllers
 
         public string FindClientByKeyWord(string key)
         {
-            List<ClientModel> clientModels = clientsService.GetClientsListSortedByName().ToModelList();
-            List<ClientModel> clients = clientModels.FindAll(client => Regex.IsMatch(client.Name, key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.LastName, key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.Age.ToString(), key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.IdentificationCode, key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.BankAccountNumber, key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.Email, key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.City, key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.RealtyType, key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.RoomsNumber.ToString(), key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.Area.ToString(), key, RegexOptions.IgnoreCase) ||
-                                                                       Regex.IsMatch(client.Budget.ToString(), key, RegexOptions.IgnoreCase));
-
-            string res = "";
-            foreach (var client in clients)
+            try
             {
-                if(res != "")
-                    res += "\n-----------------------\n\n";
+                List<ClientModel> clientModels = clientsService.GetClientsListSortedByName().ToModelList();
+                List<ClientModel> clients = clientModels.FindAll(client => Regex.IsMatch(client.Name, key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.LastName, key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.Age.ToString(), key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.IdentificationCode, key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.BankAccountNumber, key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.Email, key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.City, key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.RealtyType, key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.RoomsNumber.ToString(), key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.Area.ToString(), key, RegexOptions.IgnoreCase) ||
+                                                                           Regex.IsMatch(client.Budget.ToString(), key, RegexOptions.IgnoreCase));
 
-                res += clientController.GetClientInfo(client.IdentificationCode);
+                string res = "";
+
+                foreach (var client in clients)
+                {
+                    if (res != "")
+                        res += "\n-----------------------\n\n";
+
+                    res += clientController.GetClientInfo(client.IdentificationCode);
+                }
+
+                return res;
             }
-
-            return res;
+            catch (Exception)
+            {
+                return "По даному ключу нічого не знайдено";
+            }
         }
 
         public string FindRealtyByKeyWord(string key)
         {
-            List<RealtyModel> realtyModels = realtiesService.GetRealtiesListSortedByPrice().ToModelList();
-            List<RealtyModel> realties = realtyModels.FindAll(realty => Regex.IsMatch(realty.RealtyID, key, RegexOptions.IgnoreCase) || 
-                                                                        Regex.IsMatch(realty.City, key, RegexOptions.IgnoreCase) ||
-                                                                        Regex.IsMatch(realty.RealtyType, key, RegexOptions.IgnoreCase) ||
-                                                                        Regex.IsMatch(realty.RoomsNumber.ToString(), key, RegexOptions.IgnoreCase) ||
-                                                                        Regex.IsMatch(realty.Area.ToString(), key, RegexOptions.IgnoreCase) ||
-                                                                        Regex.IsMatch(realty.Price.ToString(), key, RegexOptions.IgnoreCase));
-
-            string res = "";
-            foreach (var realty in realties)
+            try
             {
-                if (res != "")
-                    res += "\n-----------------------\n\n";
+                List<RealtyModel> realtyModels = realtiesService.GetRealtiesListSortedByPrice().ToModelList();
+                List<RealtyModel> realties = realtyModels.FindAll(realty => Regex.IsMatch(realty.RealtyID, key, RegexOptions.IgnoreCase) ||
+                                                                            Regex.IsMatch(realty.City, key, RegexOptions.IgnoreCase) ||
+                                                                            Regex.IsMatch(realty.RealtyType, key, RegexOptions.IgnoreCase) ||
+                                                                            Regex.IsMatch(realty.RoomsNumber.ToString(), key, RegexOptions.IgnoreCase) ||
+                                                                            Regex.IsMatch(realty.Area.ToString(), key, RegexOptions.IgnoreCase) ||
+                                                                            Regex.IsMatch(realty.Price.ToString(), key, RegexOptions.IgnoreCase));
 
-                res += realtyController.GetRealtyInfo(realty.RealtyID);
+                string res = "";
+                foreach (var realty in realties)
+                {
+                    if (res != "")
+                        res += "\n-----------------------\n\n";
+
+                    res += realtyController.GetRealtyInfo(realty.RealtyID);
+                }
+
+                return res;
             }
-
-            return res;
+            catch (Exception)
+            {
+                return "По даному ключу нічого не знайдено";
+            }
         }
 
         public string FindAllByKeyWord(string key)
@@ -68,6 +85,9 @@ namespace API.Controllers
             string res = "";
 
             res += FindClientByKeyWord(key);
+
+            res += "\n-----------------\n\n";
+
             res += FindRealtyByKeyWord(key);
 
             return res;
@@ -75,71 +95,78 @@ namespace API.Controllers
 
         public string FindByClientParameters(string Name, string LastName, int Age, string IdentificationCode, string BankAccountNumber, string Email, string City, string RealtyType, int RoomsNumber, double Area, int Budget)
         {
-            List<ClientModel> clientModels = clientsService.GetClientsListSortedByName().ToModelList();
-            int countParametrs = 0;
-            int countOfCoincidence = 0;
-            string res = "";
-
-            if (Name != "")
-                countParametrs++;
-            if (LastName != "")
-                countParametrs++;
-            if (Age != 0)
-                countParametrs++;
-            if (IdentificationCode != "")
-                countParametrs++;
-            if (BankAccountNumber != "")
-                countParametrs++;
-            if (Email != "")
-                countParametrs++;
-            if (City != "")
-                countParametrs++;
-            if (RealtyType != "")
-                countParametrs++;
-            if (RoomsNumber != 0)
-                countParametrs++;
-            if (Area != 0)
-                countParametrs++;
-            if (Budget != 0)
-                countParametrs++;
-
-            foreach (var client in clientModels)
+            try
             {
-                if (client.Name == Name)
-                    countOfCoincidence++;
-                if (client.LastName == LastName)
-                    countOfCoincidence++;
-                if (client.Age == Age)
-                    countOfCoincidence++;
-                if (client.IdentificationCode == IdentificationCode)
-                    countOfCoincidence++;
-                if (client.BankAccountNumber == BankAccountNumber)
-                    countOfCoincidence++;
-                if (client.City == City)
-                    countOfCoincidence++;
-                if (client.RealtyType == RealtyType)
-                    countOfCoincidence++;
-                if (client.RoomsNumber == RoomsNumber)
-                    countOfCoincidence++;
-                if (client.Area == Area)
-                    countOfCoincidence++;
-                if (client.Budget == Budget)
-                    countOfCoincidence++;
+                List<ClientModel> clientModels = clientsService.GetClientsListSortedByName().ToModelList();
+                int countParametrs = 0;
+                int countOfCoincidence = 0;
+                string res = "";
 
-                if (countOfCoincidence == countParametrs)
+                if (Name != "")
+                    countParametrs++;
+                if (LastName != "")
+                    countParametrs++;
+                if (Age != 0)
+                    countParametrs++;
+                if (IdentificationCode != "")
+                    countParametrs++;
+                if (BankAccountNumber != "")
+                    countParametrs++;
+                if (Email != "")
+                    countParametrs++;
+                if (City != "")
+                    countParametrs++;
+                if (RealtyType != "")
+                    countParametrs++;
+                if (RoomsNumber != 0)
+                    countParametrs++;
+                if (Area != 0)
+                    countParametrs++;
+                if (Budget != 0)
+                    countParametrs++;
+
+                foreach (var client in clientModels)
                 {
-                    if (res != "")
+                    if (client.Name == Name)
+                        countOfCoincidence++;
+                    if (client.LastName == LastName)
+                        countOfCoincidence++;
+                    if (client.Age == Age)
+                        countOfCoincidence++;
+                    if (client.IdentificationCode == IdentificationCode)
+                        countOfCoincidence++;
+                    if (client.BankAccountNumber == BankAccountNumber)
+                        countOfCoincidence++;
+                    if (client.City == City)
+                        countOfCoincidence++;
+                    if (client.RealtyType == RealtyType)
+                        countOfCoincidence++;
+                    if (client.RoomsNumber == RoomsNumber)
+                        countOfCoincidence++;
+                    if (client.Area == Area)
+                        countOfCoincidence++;
+                    if (client.Budget == Budget)
+                        countOfCoincidence++;
+
+                    if (countOfCoincidence == countParametrs)
                     {
-                        res += "\n-----------------------\n\n";
+                        if (res != "")
+                        {
+                            res += "\n-----------------------\n\n";
+                        }
+
+                        res += clientController.GetClientInfo(client.IdentificationCode);
                     }
 
-                    res += clientController.GetClientInfo(client.IdentificationCode);
+                    countOfCoincidence = 0;
                 }
 
-                countOfCoincidence = 0;
+                return res;
             }
-
-            return res;
+            catch (Exception)
+            {
+                return "По даному ключу нічого не знайдено";
+            }
         }
     }
 }
